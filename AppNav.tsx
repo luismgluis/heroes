de78b3a3f86f98a984e5c709b5a8c067a968117d
Luis Miguel Grajales <grajales805@gmail.com>
2021-03-08 16:31:02 -0500

    base app created

---
+++ AppNav.tsx
@@ -0,0 +1,11 @@
+import { Layout, Text } from "@ui-kitten/components";
+import React from "react";
+const TAG = "";
+const AppNav = () => {
+  return (
+    <Layout>
+      <Text>Hola from AppNav</Text>
+    </Layout>
+  );
+};
+export default AppNav;
