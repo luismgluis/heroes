db7d0dabe68390c1186bee53d6038c7e20c5ae2d
Luis Miguel Grajales <grajales805@gmail.com>
2021-03-09 05:06:38 -0500

    add combat buttons and fight logic

--- src/components/Battle/Battle.tsx
+++ src/components/Battle/Battle.tsx
@@ -1,28 +1,93 @@
 import { Button, Layout, Text, useTheme } from "@ui-kitten/components";
-import React from "react";
-import { Dimensions, Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Dimensions, Image, StyleSheet, View } from "react-native";
 import HeroType from "../../libs/types/HeroType";
 import Panel from "../Panel/Panel";
 import HeroCard from "./../HeroCard/HeroCard";
 const TAG = "BATTLE";
+const cAlert = (title, msj) => {
+  Alert.alert(
+    title,
+    msj,
+    [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+    { cancelable: false },
+  );
+};
+const actionRun = (hero1: HeroType, hero2: HeroType) => {
+  const p1 = hero1.powerstats;
+  const p2 = hero2.powerstats;
+  if (p1.speed == p2.speed) {
+    cAlert("it's a tie", "Now change to another hero");
+    return;
+  }
+  if (p1.speed > p2.speed) {
+    cAlert(`${hero1.name} wins`, `${p1.speed} is greater than ${p2.speed}`);
+    return;
+  }
+  cAlert(`${hero2.name} wins`, `${p2.speed} is greater than ${p1.speed}`);
+};
+const actionFight = (hero1: HeroType, hero2: HeroType) => {
+  const p1 = hero1.powerstats;
+  const p2 = hero2.powerstats;
+  if (p1.power == p2.power) {
+    cAlert("it's a tie", "Now change to another hero");
+    return;
+  }
+  if (p1.power > p2.power) {
+    cAlert(`${hero1.name} wins`, `${p1.power} is greater than ${p2.power}`);
+    return;
+  }
+  cAlert(`${hero2.name} wins`, `${p2.power} is greater than ${p1.power}`);
+};
 
 const Battle = ({ navigation, route, initialBattleID = "" }) => {
   console.log(TAG, navigation);
-  const aHero: HeroType = new HeroType({});
+  const initialHero: HeroType = new HeroType({});
+  const [actualHero1, setActualHero1] = useState(initialHero);
+  const [actualHero2, setActualHero2] = useState(initialHero);
   return (
     <Panel style={styles.container}>
       <View style={styles.panelCards}>
         <View style={styles.panelCard}>
-          <HeroCard heroData={aHero} navigation={navigation} />
+          <HeroCard
+            onHeroUpdate={setActualHero1}
+            heroData={initialHero}
+            navigation={navigation}
+          />
         </View>
         <View style={styles.panelCard}>
-          <HeroCard heroData={aHero} navigation={navigation} />
+          <HeroCard
+            onHeroUpdate={setActualHero2}
+            heroData={initialHero}
+            navigation={navigation}
+          />
         </View>
       </View>
-      <View style={styles.panelButtons}>
-        <Button style={styles.button}>RUN</Button>
-        <Button style={styles.button}>FIGHT</Button>
-      </View>
+      {actualHero1.id !== "" && actualHero2.id !== "" && (
+        <View style={styles.panelButtons}>
+          <Button
+            onPress={() => {
+              actionRun(actualHero1, actualHero2);
+            }}
+            style={styles.button}>
+            RUN
+          </Button>
+          <Button
+            onPress={() => {
+              actionFight(actualHero1, actualHero2);
+            }}
+            style={styles.button}>
+            FIGHT
+          </Button>
+        </View>
+      )}
+      {!(actualHero1.id !== "" && actualHero2.id !== "") && (
+        <View style={styles.panelButtons}>
+          <Text status="warning" category="h6">
+            Waiting fighters
+          </Text>
+        </View>
+      )}
     </Panel>
   );
 };
--- src/components/HeroCard/HeroCard.tsx
+++ src/components/HeroCard/HeroCard.tsx
@@ -18,15 +18,16 @@ const StarIcon = (props) => <Icon {...props} name="flip-outline" />;
 
 interface HeroCardProps {
   heroData: HeroType;
+  onHeroUpdate: (hero: HeroType) => void;
   navigation: any;
 }
-const HeroCard = ({ heroData, navigation }: HeroCardProps) => {
+const HeroCard = ({ heroData, onHeroUpdate, navigation }: HeroCardProps) => {
   const [heroInfo, setHeroInfo] = useState(heroData);
   if (heroData.urlImage == "") {
     heroData.urlImage =
       "https://lh3.googleusercontent.com/fife/ABSRlIoG9pumdlsLMPY2EKWZhnMdixz1XJrG1qEtz0z6a4KVGHh13UdNEJXZQv1EiRt1qRPduk5Ikxc6tplJWhtZFyfRbZ4C7vFNFuoaPquVTTWUTVrkHraLBDLI2MVihrapHEW91HyE5DQ_T-zJzkyAaYscBo6O4pmrQwL5pipBLw2pinj1bkpJQThlc4QctcdpL-9UiKsFbctpTS39B15hvT-P9ST89VWFV8qRt5fsfJLQqELcB8mqkbQ2FkVozukaGW5LY4Wslsr0gnnRQ_fucoqVzOO_4v5DiQSJd1fYFaVHrp8M236tofVnErLHuGoIpdNbEa8y_I2nB-9sPq4C18qA0fM7VvlM42YNdZTUREwumTeSnww5eebnpbZZAxkOBAlNtTfqQZ8L3FTZbgeh-Z109Wh7qDXfxr4RE9Pc1KfSjV5skj8f9f18TTRGf9GJZ67frQCeEc834A7K18ja40ghTw2wcASDQNw-FdwGGxaDyrWGViAex_O7OQQPd3YCPPiOSwxakoCBFhwqpV7jUmXBYnQX7HPF5IowJ_TDFOOrSM55I-vzY_c24A6y4JreLg7KsBZRYO5JsSDheF5AQ_0NqLJTQ1ynmzIwKxafVU47N0foSMcyO3uYoVj6igkDVM7bYtAEmGblxhwqz9TzSP-1sdPUTcCj7OM2som5FqT2xrnPahKeDuTLG76t_B3L-r1BSWV8mB3o0FsB4W_hq7JHfnS9aN04ZQ=w1538-h829-ft";
   }
-
+  onHeroUpdate(heroInfo);
   const onPress = () => {
     console.log(TAG, "pepe");
     navigation.navigate("SearchHero", {
